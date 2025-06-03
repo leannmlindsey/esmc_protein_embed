9b19372245e6b18f7af86f1e8207ede43750038c
LeAnn M. Lindsey <leannmlindsey@gmail.com>
2025-06-03 10:34:07 -0400

    modifications to main script

--- scripts/visualize_all.sh
+++ scripts/visualize_all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-keys=tsp_seq.tsv
-locks=sero_seq.tsv
+keys=../data/tsp_seq.tsv
+locks=../data/sero_k_seq.tsv
 
 #python embed_proteins.py --input_file tsp_seq.tsv --output_file keys_embeddings.npz
-#python embed_proteins.py --input_file sero_seq.tsv --output_file locks_embeddings.npz
+python ../src/embed_proteins.py --input_file $locks --output_file ../output/locks_embeddings.npz
 
-python visualize_embeddings.py --embeddings locks_embeddings.npz keys_embeddings.npz --labels locks keys --output umap_plot.png
+python ../src/visualize_embeddings.py --embeddings ../output/locks_embeddings.npz ../output/keys_embeddings.npz --labels locks keys --output ../output/k_umap_plot.png
 
-python extract_clusters.py --coords_file umap_plot.coords.csv --output_dir clusters_dbscan --method dbscan --eps 0.5
-python extract_clusters.py --coords_file umap_plot.coords.csv --output_dir clusters_kmeans --method kmeans --n_clusters 5
+python extract_clusters.py --coords_file ../output/k_umap_plot.coords.csv --output_dir ../output/clusters_dbscan --method dbscan --eps 0.5
+python extract_clusters.py --coords_file ../output/k_umap_plot.coords.csv --output_dir ../output/clusters_kmeans --method kmeans --n_clusters 5
 
 
